49a9f572ecca3a8b501fa3e9374a9f4b9d3c164a
Wasi Master <63045920+wasi-master@users.noreply.github.com>
2025-08-29 00:49:05 +0600

    pwa support

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'event-scanner-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './html5-qrcode.min.js',
+  './sample_data.json',
+  // Add more assets if needed
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+    ))
+  );
+});
